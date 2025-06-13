f2e168b937424da90c77a231d2aab9f7ca652c3e
Shade <anhvu_2027@depauw.edu>
2025-06-13 15:19:04 +0700

    Fix bugs

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ function displayOrHideElement(elementName) {
 
     if (element) {
         const currentDisplay = window.getComputedStyle(element).display;
+        const fragTableButton = document.querySelector(".frag-table-button");
 
         if (currentDisplay === "block") {
             element.style.display = "none";
+            fragTableButton.innerText = "Open Fragment Table"
         }
         else {
+            fragTableButton.innerText = "Close Fragment Table"
             element.style.display = "block";
         }
     } else {
@@ -20,9 +23,11 @@ function clickout(elementName) {
 
     if (element) {
         const currentDisplay = window.getComputedStyle(element).display;
+        const fragTableButton = document.querySelector(".frag-table-button");
 
         if (currentDisplay === "block") {
             element.style.display = "none";
+            fragTableButton.innerText = "Open Fragment Table"
         }
     } else {
         console.warn(`Element with selector "${elementName}" not found.`);
@@ -51,39 +56,53 @@ function dragElement(elementName, elementHeader) {
 
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.querySelector(elementHeader)) {
-        // if present, the header is where you move the DIV from:
         document.querySelector(elementHeader).onmousedown = dragMouseDown;
     } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
         element.onmousedown = dragMouseDown;
     }
 
     function dragMouseDown(e) {
         e = e || window.event;
         e.preventDefault();
-        // get the mouse cursor position at startup:
+        
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
     }
 
     function elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
+
+        pos1 = pos3 - e.clientX; 
+        pos2 = pos4 - e.clientY; 
+
         pos3 = e.clientX;
         pos4 = e.clientY;
-        // set the element's new position:
-        element.style.top = (element.offsetTop - pos2) + "px";
-        element.style.left = (element.offsetLeft - pos1) + "px";
+
+        let newTop = element.offsetTop - pos2;
+        let newLeft = element.offsetLeft - pos1;
+
+        const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+        const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+        const elementWidth = element.offsetWidth;
+        const elementHeight = element.offsetHeight;
+
+        const minX = 0; 
+        const minY = 0; 
+        const maxX = viewportWidth - elementWidth; 
+        const maxY = viewportHeight - elementHeight; 
+
+        newLeft = Math.max(minX, Math.min(newLeft, maxX));
+        newTop = Math.max(minY, Math.min(newTop, maxY));
+
+        element.style.top = newTop + "px";
+        element.style.left = newLeft + "px";
     }
 
     function closeDragElement() {
-        // stop moving when mouse button is released:
         document.onmouseup = null;
         document.onmousemove = null;
     }
@@ -94,3 +113,4 @@ function dragElement(elementName, elementHeader) {
 window.onbeforeunload = function() {
   return "Data will be lost if you leave the page, are you sure?";
 };
+
