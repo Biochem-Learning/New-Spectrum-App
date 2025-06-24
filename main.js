d57d4f50b55558d35addf22807d1e969d29203ff
Shade <anhvu_2027@depauw.edu>
2025-06-24 19:28:13 +0700

    Clean code

--- main.js
+++ main.js
@@ -3,44 +3,10 @@ const MOL_CANVAS_HEIGHT = 90;
 const SPEC_CANVAS_WIDTH = 95;
 const SPEC_CANVAS_HEIGHT = 95;
 
+const SPEC_DESCRIPTION_TEXTBOX =  document.querySelector(".spec-desc"); 
+
 let displayingMol = "Ethanol"
 let viewingMode = "MS"
-
-function displayOrHideElement(elementName) {
-    const element = document.querySelector(elementName);
-
-    if (element) {
-        const currentDisplay = window.getComputedStyle(element).display;
-        const fragTableButton = document.querySelector(".frag-table-button");
-
-        if (currentDisplay === "block") {
-            element.style.display = "none";
-            fragTableButton.innerText = "Open Fragment Table"
-        }
-        else {
-            fragTableButton.innerText = "Close Fragment Table"
-            element.style.display = "block";
-        }
-    } else {
-        console.warn(`Element with selector "${elementName}" not found.`);
-    }
-}
-
-function clickout(elementName) {
-    const element = document.querySelector(elementName);
-
-    if (element) {
-        const currentDisplay = window.getComputedStyle(element).display;
-        const fragTableButton = document.querySelector(".frag-table-button");
-
-        if (currentDisplay === "block") {
-            element.style.display = "none";
-            fragTableButton.innerText = "Open Fragment Table"
-        }
-    } else {
-        console.warn(`Element with selector "${elementName}" not found.`);
-    }
-}
 
 document.querySelector(".frag-table-button").addEventListener("click", function() {
     event.stopPropagation()
@@ -58,129 +24,10 @@ document.addEventListener("click", function() {
 ///drag
 dragElement(".frag-table", ".f-t-general-instr");
 
-function dragElement(elementName, elementHeader) {
-    let element = document.querySelector(elementName);
-
-    if (!element) {
-        console.error(`Element with selector "${elementName}" not found. Cannot make it draggable.`);
-        return;
-    }
-
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-    let dragHandle = element;
-    if (elementHeader) { 
-        const headerElement = document.querySelector(elementHeader);
-        if (headerElement) {
-            dragHandle = headerElement; 
-        } else {
-            console.warn(`Element header with selector "${elementHeader}" not found. Using main element as drag handle.`);
-        }
-    }
-
-    element.style.cursor = 'grab';
-    element.style.touchAction = 'none';
-
-    dragHandle.onmousedown = dragStart;   
-    dragHandle.ontouchstart = dragStart;
-
-
-    // --- Core Drag Functions ---
-    function dragStart(e) {
-        e = e || window.event;
-        e.preventDefault(); 
-
-        let currentClientX, currentClientY;
-        if (e.type === 'touchstart') {
-            const touch = e.touches[0];
-            currentClientX = touch.clientX;
-            currentClientY = touch.clientY;
-        } else { 
-            currentClientX = e.clientX;
-            currentClientY = e.clientY;
-        }
-
-        pos3 = currentClientX; 
-        pos4 = currentClientY; 
-
-        if (e.type === 'touchstart') {
-            document.ontouchend = dragEnd;
-            document.ontouchmove = elementDrag;
-        } else {
-            document.onmouseup = dragEnd;
-            document.onmousemove = elementDrag;
-        }
-
-        element.style.cursor = 'grabbing'; 
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-
-        let currentClientX, currentClientY;
-        if (e.type === 'touchmove') {
-            e.preventDefault(); 
-            const touch = e.touches[0];
-            currentClientX = touch.clientX;
-            currentClientY = touch.clientY;
-        } else { // mousemove
-            currentClientX = e.clientX;
-            currentClientY = e.clientY;
-        }
-
-        pos1 = pos3 - currentClientX;
-        pos2 = pos4 - currentClientY;
-
-        pos3 = currentClientX;
-        pos4 = currentClientY;
-
-        let newTop = element.offsetTop - pos2;
-        let newLeft = element.offsetLeft - pos1;
-
-        const viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        const viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-        const elementWidth = element.offsetWidth;
-        const elementHeight = element.offsetHeight;
-
-        const minX = 0;
-        const minY = 0;
-        const maxX = viewportWidth - elementWidth;
-        const maxY = viewportHeight - elementHeight;
-
-        newLeft = Math.max(minX, Math.min(newLeft, maxX));
-        newTop = Math.max(minY, Math.min(newTop, maxY));
-
-        element.style.top = newTop + "px";
-        element.style.left = newLeft + "px";
-    }
-
-    function dragEnd() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-        document.ontouchend = null;
-        document.ontouchmove = null;
-    }
-}
-
-
 /// Change alert
 window.onbeforeunload = function() {
   return "Data will be lost if you leave the page, are you sure?";
 };
-
-/// Display molecules and spectral graph
-async function getData(path = "") {
-    const response = await fetch(path);
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status} - Could not load file from URL.`);
-    }
-
-    const contentString = await response.text();
-    
-    return contentString;
-}
 
 /// !!!NOTICE: For anyone who try to work on this part, the original ChemDoodle lib only support 
 
@@ -206,47 +53,14 @@ async function setUpCanvas(path='', molCanvasId, specCanvasId) {
 const MOL_CANVAS_ID = "sample_molecule";
 const SPEC_CANVAS_ID = "sample_spectrum";
 let canvases = setUpCanvas(
-    'data/Spectra/MS/' + displayingMol + 'MS.jdx',
+    'data/Spectra/' + viewingMode + '/' + displayingMol + viewingMode + '.jdx',
     MOL_CANVAS_ID,
     SPEC_CANVAS_ID
 );
 
 
-function getDivAndParentEl(divSelector) {
-    div = document.querySelector(divSelector);
-
-    if (!div) {
-        console.error(`Element with selector "${divSelector}" not found.`);
-        return null;
-    }
-
-    parent = div.parentNode;
-    
-    if (!parent) {
-        console.error(`Element with selector "${divSelector}" has no parent.`);
-        return null;
-    }
-
-    return {
-        element: div,
-        parent: parent
-
-    };
-}
-
-function percentage(divSelector, percentage, dimension) {
-    let divAndParent = getDivAndParentEl(divSelector)
-
-    if (!divAndParent) {
-        return
-    }
-
-    let parent = divAndParent.parent;
-
-    parentDimension = (dimension === "width") ? parent.offsetWidth : parent.offsetHeight 
-    caculatedDimension = parentDimension * percentage / 100;
-
-    return caculatedDimension
+if (SPEC_DESCRIPTION_TEXTBOX.innerText === "") {
+    alert("Go fuck yourself")
 }
 
 async function initializeCanvas(canvas, url="") {
@@ -291,21 +105,20 @@ async function createCompoundSubmenu(menuFilePath) {
             ddContentWrapper.appendChild(subItem);
 
             subItem.addEventListener('click', function(event) {
-                event.preventDefault(); // Prevent default link behavior if you're handling navigation with JS
+                event.preventDefault(); 
                 displayingMol = itemArray[i];
+
                 canvases = setUpCanvas('data/Spectra/MS/' + displayingMol + 'MS.jdx',/// Might change to return instead
                     MOL_CANVAS_ID,
                     SPEC_CANVAS_ID
                 )
 
-                // setupSpectrumInteractivity(canvases, "MS", displayingMol)
                 displayGeneralText(displayingMol)
             });
         }
     })
     .catch(error => console.error('Error reading file:', error));
 
-    dropdownMenu.appendChild(ddContentWrapper)
 }
 
 
@@ -326,7 +139,6 @@ document.querySelectorAll(".nav-bar-section").forEach(button => {
             SPEC_CANVAS_ID
         ) 
         viewingMode = this.id;
-        // setupSpectrumInteractivity(canvases, this.id, displayingMol)
         displayGeneralText(displayingMol, this.id)
     });
 })
@@ -338,37 +150,35 @@ async function getDataJSON(jsonPath) {
     return dataObject;
 }
 
-displayGeneralText("Ethanol")
+displayGeneralText(displayingMol)
 
 ///Not good, need rewrite
-async function displayGeneralText(molecule,mode="") {
+async function displayGeneralText(molecule,mode="",textBoxEl=SPEC_DESCRIPTION_TEXTBOX) {
     const dataJSON = await getDataJSON("data/Spectra/SpecDescription/" + molecule +".json");
-    let textBox = document.querySelector(".spec-desc"); 
     switch (mode) {
         case "":
-            textBox.innerText = dataJSON.description;
+            textBoxEl.innerText = dataJSON.description;
             break
         case "MS":
-            textBox.innerText = dataJSON.spectra_info.MS.general_description;
+            textBoxEl.innerText = dataJSON.spectra_info.MS.general_description;
             break
         case "IR":
-            textBox.innerText = dataJSON.spectra_info.IR.general_description;
+            textBoxEl.innerText = dataJSON.spectra_info.IR.general_description;
             break
         case "HNMR":
-            textBox.innerText = dataJSON.spectra_info.HNMR.general_description;
+            textBoxEl.innerText = dataJSON.spectra_info.HNMR.general_description;
             break
         case "CNMR":
-            textBox.innerText = dataJSON.spectra_info.CNMR.general_description;
+            textBoxEl.innerText = dataJSON.spectra_info.CNMR.general_description;
             break
     }
 }
 
 
-async function displayTextWhenHovered(hoveredEl, mode="MS", mol="Ethanol") {
-    const dataJSON = await getDataJSON("data/Spectra/SpecDescription/" + displayingMol + ".json");
-    let textBox = document.querySelector(".spec-desc"); 
+async function displayTextWhenHovered(hoveredEl, mode, mol, textBoxEl) {
+    const dataJSON = await getDataJSON("data/Spectra/SpecDescription/" + mol + ".json");
 
-    if (!textBox) {
+    if (!textBoxEl) {
         console.error("Error: Element with class '.spec-desc' not found.");
         return; 
     }
@@ -395,7 +205,7 @@ async function displayTextWhenHovered(hoveredEl, mode="MS", mol="Ethanol") {
             for(let i = 0; i < peaksFromJSON.length; i += 1)  {
                 
                 if (hoveredEl.x == peaksFromJSON[i].x) { 
-                    textBox.innerText = peaksFromJSON[i].description;
+                    textBoxEl.innerText = peaksFromJSON[i].description;
                 }
             }
         } else {
@@ -408,9 +218,8 @@ async function displayTextWhenHovered(hoveredEl, mode="MS", mol="Ethanol") {
     }
 }
 
-function removeTextWhenMoveout() {
-    let textBox = document.querySelector(".spec-desc")
-    textBox.innerText = "";
+function removeTextWhenMoveout(textBoxEl) {
+    textBoxEl.innerText = "";
 }
 
 /// Things need to be done:
---
+++ utils.js
@@ -0,0 +1,190 @@
+function displayOrHideElement(elementName) {
+    const element = document.querySelector(elementName);
+
+    if (element) {
+        const currentDisplay = window.getComputedStyle(element).display;
+        const fragTableButton = document.querySelector(".frag-table-button");
+
+        if (currentDisplay === "block") {
+            element.style.display = "none";
+            fragTableButton.innerText = "Open Fragment Table"
+        }
+        else {
+            fragTableButton.innerText = "Close Fragment Table"
+            element.style.display = "block";
+        }
+    } else {
+        console.warn(`Element with selector "${elementName}" not found.`);
+    }
+}
+
+function clickout(elementName) {
+    const element = document.querySelector(elementName);
+
+    if (element) {
+        const currentDisplay = window.getComputedStyle(element).display;
+        const fragTableButton = document.querySelector(".frag-table-button");
+
+        if (currentDisplay === "block") {
+            element.style.display = "none";
+            fragTableButton.innerText = "Open Fragment Table"
+        }
+    } else {
+        console.warn(`Element with selector "${elementName}" not found.`);
+    }
+}
+
+function dragElement(elementName, elementHeader) {
+    let element = document.querySelector(elementName);
+
+    if (!element) {
+        console.error(`Element with selector "${elementName}" not found. Cannot make it draggable.`);
+        return;
+    }
+
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    let dragHandle = element;
+    if (elementHeader) { 
+        const headerElement = document.querySelector(elementHeader);
+        if (headerElement) {
+            dragHandle = headerElement; 
+        } else {
+            console.warn(`Element header with selector "${elementHeader}" not found. Using main element as drag handle.`);
+        }
+    }
+
+    element.style.cursor = 'grab';
+    element.style.touchAction = 'none';
+
+    dragHandle.onmousedown = dragStart;   
+    dragHandle.ontouchstart = dragStart;
+
+
+    // --- Core Drag Functions ---
+    function dragStart(e) {
+        e = e || window.event;
+        e.preventDefault(); 
+
+        let currentClientX, currentClientY;
+        if (e.type === 'touchstart') {
+            const touch = e.touches[0];
+            currentClientX = touch.clientX;
+            currentClientY = touch.clientY;
+        } else { 
+            currentClientX = e.clientX;
+            currentClientY = e.clientY;
+        }
+
+        pos3 = currentClientX; 
+        pos4 = currentClientY; 
+
+        if (e.type === 'touchstart') {
+            document.ontouchend = dragEnd;
+            document.ontouchmove = elementDrag;
+        } else {
+            document.onmouseup = dragEnd;
+            document.onmousemove = elementDrag;
+        }
+
+        element.style.cursor = 'grabbing'; 
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+
+        let currentClientX, currentClientY;
+        if (e.type === 'touchmove') {
+            e.preventDefault(); 
+            const touch = e.touches[0];
+            currentClientX = touch.clientX;
+            currentClientY = touch.clientY;
+        } else { // mousemove
+            currentClientX = e.clientX;
+            currentClientY = e.clientY;
+        }
+
+        pos1 = pos3 - currentClientX;
+        pos2 = pos4 - currentClientY;
+
+        pos3 = currentClientX;
+        pos4 = currentClientY;
+
+        let newTop = element.offsetTop - pos2;
+        let newLeft = element.offsetLeft - pos1;
+
+        const viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+        const elementWidth = element.offsetWidth;
+        const elementHeight = element.offsetHeight;
+
+        const minX = 0;
+        const minY = 0;
+        const maxX = viewportWidth - elementWidth;
+        const maxY = viewportHeight - elementHeight;
+
+        newLeft = Math.max(minX, Math.min(newLeft, maxX));
+        newTop = Math.max(minY, Math.min(newTop, maxY));
+
+        element.style.top = newTop + "px";
+        element.style.left = newLeft + "px";
+    }
+
+    function dragEnd() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+        document.ontouchend = null;
+        document.ontouchmove = null;
+    }
+}
+
+async function getData(path = "") {
+    const response = await fetch(path);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status} - Could not load file from URL.`);
+    }
+
+    const contentString = await response.text();
+    
+    return contentString;
+}
+
+function getDivAndParentEl(divSelector) {
+    div = document.querySelector(divSelector);
+
+    if (!div) {
+        console.error(`Element with selector "${divSelector}" not found.`);
+        return null;
+    }
+
+    parent = div.parentNode;
+    
+    if (!parent) {
+        console.error(`Element with selector "${divSelector}" has no parent.`);
+        return null;
+    }
+
+    return {
+        element: div,
+        parent: parent
+
+    };
+}
+
+function percentage(divSelector, percentage, dimension) {
+    let divAndParent = getDivAndParentEl(divSelector)
+
+    if (!divAndParent) {
+        return
+    }
+
+    let parent = divAndParent.parent;
+
+    parentDimension = (dimension === "width") ? parent.offsetWidth : parent.offsetHeight 
+    caculatedDimension = parentDimension * percentage / 100;
+
+    return caculatedDimension
+}
+
